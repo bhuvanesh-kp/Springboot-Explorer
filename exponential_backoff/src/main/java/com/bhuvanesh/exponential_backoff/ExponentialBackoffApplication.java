3734a5b72f3bcc5e470fad63893c5fb432e63a22
Arques <bhuvaneshkpb@gmail.com>
2026-05-30 12:14:56 +0530

    testing exponential backoff

---
+++ exponential_backoff/src/main/java/com/bhuvanesh/exponential_backoff/ExponentialBackoffApplication.java
@@ -0,0 +1,13 @@
+package com.bhuvanesh.exponential_backoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExponentialBackoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExponentialBackoffApplication.class, args);
+	}
+
+}
---
+++ exponential_backoff/src/test/java/com/bhuvanesh/exponential_backoff/ExponentialBackoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuvanesh.exponential_backoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExponentialBackoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
