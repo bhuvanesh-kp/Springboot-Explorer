1c3ec9607223fdc94215c74976e5e7145e24a231
Arques <bhuvaneshkpb@gmail.com>
2026-05-31 16:43:32 +0530

    testing commandline for creating boiler plate folder structure

---
+++ commandline/src/test/java/com/bhuvanesh/commandline/CommandlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuvanesh.commandline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
